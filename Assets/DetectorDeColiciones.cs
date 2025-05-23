115f4e2251f5ffd30d28a84addea34b09cb28e70
camilorosem <49398937@est.ort.edu.ar>
2025-05-23 10:35:08 -0300

    hacer las coliciones que no llegue.

---
+++ Assets/DetectorDeColiciones.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DetectorDeColiciones : MonoBehaviour
+{
+   private void OnCollisionEnter(Collision collision)
+    {
+        UnityEngine.Debug.Log("Contacto");
+        Destroy(collision.gameObject);
+    }
+}
