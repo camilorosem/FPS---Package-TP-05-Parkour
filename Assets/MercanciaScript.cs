9437146a553bb474ef551259710f85b85b3b0c6a
camilorosem <49398937@est.ort.edu.ar>
2025-05-30 10:30:53 -0300

    .

---
+++ Assets/MercanciaScript.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MercanciaScript : MonoBehaviour
+{
+    public float cost = 5;
+    public MoneyManager moneyManager;
+
+    private void Start()
+    {
+        moneyManager = FindObjectOfType<MoneyManager>();
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.CompareTag("Player"))
+        {
+            moneyManager.UpdateMoney(-cost);
+            Destroy(gameObject);
+        }
+    }
+}
